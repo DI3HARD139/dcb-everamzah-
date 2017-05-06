337eda3303fbe1a6777292a2fb991d77a7ffe4f8
elite <cessna151@hotmail.com>
2017-05-06 14:13:31 -0400

    Update IRC mod

--- mods/irc_modpack/irc/config.lua
+++ mods/irc_modpack/irc/config.lua
@@ -4,7 +4,7 @@
 
 irc.config = {}
 
-local function setting(stype, name, default, required)
+local function setting(stype, name, default)
 	local value
 	if stype == "bool" then
 		value = minetest.setting_getbool("irc."..name)
@@ -14,10 +14,6 @@ local function setting(stype, name, default, required)
 		value = tonumber(minetest.setting_get("irc."..name))
 	end
 	if value == nil then
-		if required then
-			error("Required configuration option irc."..
-				name.." missing.")
-		end
 		value = default
 	end
 	irc.config[name] = value
@@ -27,16 +23,15 @@ end
 -- BASIC USER SETTINGS --
 -------------------------
 
-setting("string", "nick", nil, true) -- Nickname
-setting("string", "server", nil, true) -- Server address to connect to
-setting("number", "port", 6667) -- Server port to connect to
+setting("string", "nick") -- Nickname (default "MT-<hash>", <hash> 6 random hexidecimal characters)
+setting("string", "server", "irc.freenode.net") -- Server to connect on joinplayer
+setting("number", "port", 6667) -- Port to connect on joinplayer
 setting("string", "NSPass") -- NickServ password
 setting("string", "sasl.user", irc.config.nick) -- SASL username
 setting("string", "sasl.pass") -- SASL password
-setting("string", "channel", nil, true) -- Channel to join
+setting("string", "channel", "##mt-irc-mod") -- Channel to join
 setting("string", "key") -- Key for the channel
 setting("bool",   "send_join_part", true) -- Whether to send player join and part messages to the channel
-setting("bool",   "send_kicks", false) -- Whether to send player kicked messages to the channel
 
 -----------------------
 -- ADVANCED SETTINGS --
@@ -45,10 +40,50 @@ setting("bool",   "send_kicks", false) -- Whether to send player kicked messages
 setting("string", "password") -- Server password
 setting("bool",   "secure", false) -- Enable a TLS connection, requires LuaSEC
 setting("number", "timeout", 60) -- Underlying socket timeout in seconds.
-setting("number", "reconnect", 600) -- Time between reconnection attempts, in seconds.
-setting("string", "command_prefix") -- Prefix to use for bot commands
+setting("string", "command_prefix", "/") -- Prefix to use for bot commands
 setting("bool",   "debug", false) -- Enable debug output
 setting("bool",   "enable_player_part", true) -- Whether to enable players joining and parting the channel
 setting("bool",   "auto_join", true) -- Whether to automatically show players in the channel when they join
 setting("bool",   "auto_connect", true) -- Whether to automatically connect to the server on mod load
+
+-- Generate a random nickname if one isn't specified.
+if not irc.config.nick then
+	local pr = PseudoRandom(os.time())
+	-- Workaround for bad distribution in minetest PRNG implementation.
+	irc.config.nick = ("MT-%02X%02X%02X"):format(
+		pr:next(0, 255),
+		pr:next(0, 255),
+		pr:next(0, 255)
+	)
+end
+
+
+local config_file = minetest.get_worldpath() .. "/irc.txt"
+
+function irc.save_config()
+	local input, err = io.open(config_file, "w")
+	if input then
+		local conf = {server = irc.config.server}
+		input:write(minetest.serialize(conf))
+		input:close()
+	else
+		minetest.log("error", "open(" .. config_file .. ", 'w') failed: " .. err)
+	end
+end
+
+function irc.load_config()
+	local file = io.open(config_file, "r")
+	local settings = {}
+	if file then
+		settings = minetest.deserialize(file:read("*all"))
+		file:close()
+		if settings and type(settings) == "table" then
+			if settings["server"] ~= nil then
+				 irc.config.server = settings["server"]
+			end
+		end
+	end
+end
+
+irc.load_config()
 
