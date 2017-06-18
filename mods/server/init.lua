7c31593caf46bd27dc9d8c26f92e12cb6fcba5e2
DI3HARD139 <dudes9992@yahoo.com>
2017-06-18 09:26:16 -0400

    Patch added for re-enabling 'sneak' on 0.4.16

--- mods/server/init.lua
+++ mods/server/init.lua
@@ -8,3 +8,4 @@ dofile(path .. "/namesperip.lua") -- Krock
 dofile(path .. "/mapfix.lua") -- Gael-de-Sailly
 dofile(path .. "/clearobjects.lua") -- DI3HARD139/red-001
 dofile(path .. "/chat.lua") -- DI3HARD139
+dofile(path .. "/sneak.lua") -- PilzAdam
